7209528afbfede60e8c29b6a042a6daf26c5acab
sudhee <sudheendra.teamchimera@gmail.com>
2019-06-18 15:50:31 +0530

    added header file

---
+++ upload_voltage/DischargeBattery.h
@@ -0,0 +1,6 @@
+#define LOAD_560 16
+#define LOAD_232 5
+#define LOAD_116 4
+#define LOAD_047 13
+#define DUT      15
+#define FLASH_BUTTON 0
--- upload_voltage/upload_voltage.ino
+++ upload_voltage/upload_voltage.ino
@@ -4,7 +4,6 @@
 // Licensed under the MIT license.
 //
 // All text above must be included in any redistribution.
-
 /************************** Configuration ***********************************/
 // edit the config.h tab and enter your Adafruit IO credentials
 // and any additional configuration needed for WiFi, cellular,
@@ -12,6 +11,7 @@
 /****************************************************************************/
 
 #include "config.h"
+#include "DischargeBattery.h"
 
 #define Vin A0  //input: battery voltage 
 const float VLowerCutOff = 2.1;  //define the lower cut off voltage or the end of discharge voltage for the battery
@@ -19,24 +19,24 @@ int voltage = 0;
 int last = -1;
 int Vin_max = 3.3;
 int Vin_min = 0;
-boolean flag = 1;      
+boolean ONCE_DISCHARGED = 0;      
 
 AdafruitIO_Feed *analog = io.feed("analog"); //Set up the feed. Replace "analog" in this line with the name of the feed that you created
 
 void setup() {
   //set pinMode
-  pinMode(16,OUTPUT);  //pin D0, load resistance of 560ohm
-  pinMode(5,OUTPUT);  //pin D1, load resistance of 330ohm
-  pinMode(4,OUTPUT);  //pin D2, load resistance of 100ohm
-  pinMode(13,OUTPUT); //pin D7, load resistance of 047ohm
-  pinMode(15,OUTPUT); //pin D8, load is the Device Under Test (DUT)
-  pinMode(0,INPUT_PULLUP);  //pin D3, to which the 'Flash' push button is connected.
+  pinMode(LOAD_560,OUTPUT);  //pin D0, load resistance of 560ohm
+  pinMode(LOAD_232,OUTPUT);  //pin D1, load resistance of 232ohm
+  pinMode(LOAD_116,OUTPUT);  //pin D2, load resistance of 116ohm
+  pinMode(LOAD_047,OUTPUT); //pin D7, load resistance of 047ohm
+  pinMode(DUT,OUTPUT); //pin D8, load is the Device Under Test (DUT)
+  pinMode(FLASH_BUTTON,INPUT_PULLUP);  //pin D3, to which the 'Flash' push button is connected.
   //make sure all loads are off initially
-  digitalWrite(16,HIGH);
-  digitalWrite(5,HIGH);
-  digitalWrite(4,HIGH);
-  digitalWrite(13,HIGH);
-  digitalWrite(15,HIGH);
+  digitalWrite(LOAD_560,HIGH);
+  digitalWrite(LOAD_232,HIGH);
+  digitalWrite(LOAD_116,HIGH);
+  digitalWrite(LOAD_047,HIGH);
+  digitalWrite(DUT,HIGH);
   
   Serial.begin(115200);    //begin serial communication
 
@@ -56,7 +56,7 @@ void setup() {
   Serial.println();
   Serial.println(io.statusText());
   Serial.println();
-  digitalWrite(4,LOW);     //turn on the mosfet connecting 100ohm load
+  digitalWrite(LOAD_116,LOW);     //turn on the mosfet connecting 100ohm load
 }
 
 void loop() {
@@ -64,7 +64,7 @@ void loop() {
   // io.run(); is required for all sketches. It should always be present at the top of your loop function. It keeps the client connected to
   // io.adafruit.com, and processes any incoming data.
   io.run();
-  if(flag)  {
+  if(! ONCE_DISCHARGED)  {
   voltage = analogRead(Vin);  // grab the voltage of the battery
   float Vbat = float((Vin_max - Vin_min))*voltage/1024;
 
@@ -81,12 +81,12 @@ void loop() {
 }
   if(last < VLowerCutOff)  {   //if the battery voltage falls below the lower cut off
                       //turn OFF ALL loads when batteries got fully discharged.
-      flag=0;         //clear the flag so that it does not oscillate when terminal voltage rises slightly.
-      digitalWrite(16,HIGH);
-      digitalWrite(5,HIGH);
-      digitalWrite(4,HIGH);
-      digitalWrite(13,HIGH);
-      digitalWrite(15,HIGH); 
+      ONCE_DISCHARGED = 1;         //clear the ONCE_DISCHARGED so that it does not oscillate when terminal voltage rises slightly.
+      digitalWrite(LOAD_560,HIGH);
+      digitalWrite(LOAD_232,HIGH);
+      digitalWrite(LOAD_116,HIGH);
+      digitalWrite(LOAD_047,HIGH);
+      digitalWrite(DUT,HIGH); 
       Serial.println();
       Serial.println("Batteries got fully DISCHARGED...");
       Serial.println();
