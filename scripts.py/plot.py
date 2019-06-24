f9e64f8d9418a8248a201cc710f86c468ef70839
sudhee <sudheendra.teamchimera@gmail.com>
2019-06-24 17:03:25 +0530

    improved script

--- scripts.py/plot.py
+++ scripts.py/plot.py
@@ -2,27 +2,53 @@ from matplotlib import pyplot as plt
 from matplotlib import style
 import numpy as np
 from numpy import trapz
+
 style.use('ggplot')
 
-x,y = np.loadtxt('testdata.csv',
-		 unpack= True,
-		 delimiter = ',')
+file_name= raw_input("Enter the CSV file name: ")
+strlen=len(file_name)
+if ((file_name[strlen-4 : strlen] != ".csv") and (file_name[strlen-4 : strlen] != ".CSV")):
+	file_name += ".csv"
+Value = np.loadtxt(file_name,
+			unpack= True,
+			skiprows=1,
+			usecols=1,
+ 			delimiter = ',')
+
+start_time = np.loadtxt(file_name,
+			dtype=str,
+			delimiter = ',',
+			skiprows=1,
+			max_rows=1,
+			unpack=True,
+			usecols=3)
+
+end_time = np.loadtxt(file_name,
+			dtype=str,
+			delimiter=',',
+			usecols=3,
+			unpack=True,
+			skiprows=len(Value))
 
 fig=plt.figure()
 ax=fig.add_subplot(111)
-l=len(y)
+l=len(Value)
 x=range(0,2*l-1,2)
-mAh=trapz(y)
+mAh=trapz(Value)
 mAh /= 100
-zz=str(mAh)+"mAh\nNo. of data points ="+str(l)
-#ax.annotate(z,xy=(2.10,0),xytext=(2.10,0))
+zz = str(mAh)+"mAh\nNo. of data points:"+str(l)
 bbox_props = dict(boxstyle="square,pad=0.3", fc="cyan", ec="b", lw=2)
-ax.text(0, 1, zz, ha="left", va="center",
+ax.text(-800, 1, zz, ha="right", va="bottom",
             size=15,
             bbox=bbox_props)
-
-plt.plot(x,y,'b-')
-plt.plot(x,y/0.1,'r--')
+ax.text(0, -2.5, start_time, ha="center", va="top",
+            size=15,
+            bbox=bbox_props)
+ax.text(2*l-1, -2.5, end_time, ha="center", va="top",
+            size=15,
+            bbox=bbox_props)
+plt.plot(x,Value,'b-')
+plt.plot(x,Value/0.1,'r--')
 plt.title('Discharge Curve')
 plt.ylabel('Voltage')
 plt.xlabel('Time')
